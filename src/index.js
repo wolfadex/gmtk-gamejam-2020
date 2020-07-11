2b47f4e23a55a549eaef3eb9849fe7919b89218e
Wolfgang Schuster <wolfadex@gmail.com>
2020-07-11 12:37:03 -0600

    Add 'windows'

--- src/index.js
+++ src/index.js
@@ -1,52 +1,101 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 
 
-render(<Terminal />, document.getElementById("root"))
+render(<Game />, document.getElementById("root"))
 
-function getWord() {
+
+function Game() {
+	const [popups, setPopups] = useState([createPopup()]);
+
+	// useEffect(() => {
+
+	// }, popups)
+
+	return (
+		<>
+			<Terminal commandEntered={(cmd) => {
+				const index = popups.findIndex((word) => word === cmd);
+
+				if (index > -1) {
+					popups.splice(index, 1);
+					setPopups(popups);
+				}
+			}}/>
+			{popups.map((popup) => {
+				<Window>
+					{popup}
+				</Window>
+			})}
+		</>
+	);
+}
+
+
+function createPopup() {
   const words = [
 	"word",
 	"hack",
 	"computer",
 	"system",
   ];
-  const max = Math.floor(words.length);
-  const index = Math.floor(Math.random() * max);
-  return words[index];
+
+  const index = randomInt(0, words.length);
+  
+  return words[index]
+}
+
+function randomInt(min, max) {
+	min = Math.floor(min);
+	max = Math.floor(max);
+  	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
-function Terminal() {
+function Terminal({ commandEntered }) {
 	const [currentInput, setCurrentInput] = useState("");
-	const [currentWord, setCurrentWord] = useState(getWord())
-	const [previousWords, setPreviousWords] = useState([]);
+	const [previousInputs, setPreviousInputs] = useState([]);
 
 	return (
-		<>
+		<Window left="40" top="40">
 			<ul className="previous-commands">
-				{previousWords.map((word, i) =>
+				{previousInputs.map((word, i) =>
 					<li key={i + "__" + word}>
-						$$ word
+						word
 					</li>
 				)}
 			</ul>
-			{"$> " + currentWord} 
-			<input
-				autoFocus
-				onChange={({ target: { value } }) => setCurrentInput(value)} 
-				value={currentInput} 
-				onKeyDown={({ key }) => {
-					if (key === "Enter") {
-						if (currentInput === currentWord) {
-							setCurrentWord(getWord());
-							setPreviousWords([currentWord, ...previousWords])
+			{"$> "}
+			<div>
+				<span></span>
+				<input
+					className="terminal"
+					autoFocus
+					onChange={({ target: { value } }) => setCurrentInput(value)} 
+					value={currentInput} 
+					onKeyDown={({ key }) => {
+						if (key === "Enter") {
+							commandEntered(currentInput)
+							setPreviousInputs([currentInput, ...previousInputs])
+							setCurrentInput("");
 						}
-
-						setCurrentInput("");
-					}
-				}}
-			/>
-		</>
+					}}
+				/>
+			</div>
+		</Window>
 	)
+}
+
+function Window({ left, top, children }) {
+	const [position, setPosition] = useState({
+		left: left != null ? left : randomInt(0, window.width), 
+		top: top != null ? top : randomInt(0, window.height)
+	});
+
+	return (
+		<div style={{ left: position.left, top: position.top }} className="faux-window">
+			<div class="window-header"><button onClick={() => {}	}>X</button></div>
+			{children}
+		</div>
+	);
 }
